1a36b8f7f8a9d20fffe38d962f8e4b2a250a6e59
Konsta Majakangas <konsta@Konsta---MacBook-Air.local>
2025-06-01 19:02:50 +0300

    Show post list/info

--- posts.py
+++ posts.py
@@ -3,3 +3,17 @@ import db
 def add_post(title, description, category, user_id):
     sql = """INSERT INTO posts (title, description, category, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, category, user_id])
+
+def get_posts():
+    sql = "SELECT id, title FROM posts ORDER BY id DESC"
+    return db.query(sql)
+
+def get_post(post_id):
+    sql = """SELECT posts.title,
+                    posts.description,
+                    posts.category,
+                    users.username
+                FROM posts, users
+                WHERE posts.user_id = users.id AND
+                        posts.id = ?"""
+    return db.query(sql, [post_id])[0]
