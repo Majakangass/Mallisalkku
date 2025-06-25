c1f9d2cd6e2b57100e987d500a7807f171fba9af
Konsta Majakangas <konsta@Konsta---MacBook-Air.local>
2025-06-25 22:34:45 +0300

    Change import order

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import re
+import secrets
 import sqlite3
+
 from flask import Flask
 from flask import abort, flash, make_response, redirect, render_template, request, session
+import markupsafe
+
 import config
 import db
 import posts
 import users
-import re
-import secrets
-import markupsafe
+
+
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
